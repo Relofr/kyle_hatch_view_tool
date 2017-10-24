7b810bd9d1e6d973792e00e2939c46f36f06e2ca
Kyle Hatch <ky13h4tch@gmail.com>
2017-10-24 14:26:00 -0600

    Dev tool initial commit

---
+++ lib/kyle_hatch_view_tool.rb
@@ -0,0 +1,6 @@
+require "kyle_hatch_view_tool/version"
+require "kyle_hatch_view_tool/renderer"
+
+module KyleHatchViewTool
+
+end
---
+++ lib/kyle_hatch_view_tool/version.rb
@@ -0,0 +1,3 @@
+module KyleHatchViewTool
+  VERSION = "0.1.0"
+end
